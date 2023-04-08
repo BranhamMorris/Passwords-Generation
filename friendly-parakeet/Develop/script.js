4179bb63132570753e404a4b5c88ddf5b498054a
Branham Morris <branhammorris1@gmail.com>
2023-04-08 09:23:35 -0400

    initial commit

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,17 +1,66 @@
 // Assignment code here
+var characterLength = 8;
+var choice = [];
+var specialCharacter = ["!","@","#","$","%","^","&","*","(",")","_","-","=","+"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var number = ["1","2","3","4","5","6","7","8","9","0"];
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if(correctPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+} else {
+  passwordText.value = "";
+}
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  //generate based on prompts
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * choice.length);
+    password = password + choice [randomCharacter];
+  }
+  return password;
+}
+
+function getPrompts() {
+  choice = [];
+
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length has to be a number, 8 - 128 digits. Try again.");
+    return false;
+  }
+
+  if (confirm(" Do you want lowercase letters in the password?")) {
+    choice = choice.concat(lowerCase);
+  };
+  if (confirm("Do you want uppercase letters in the password?")) {
+    choice = choice.concat(upperCase);
+  }
+  if (confirm("Do you want special characters in the password?")) {
+    choice = choice.concat(specialCharacter);
+  }
+
+  if (confirm("Do you want numbers in the password?")) {
+    choice = choice.concat(number);
+  }
+
+}
+
+
--- friendly-parakeet/README.md
+++ friendly-parakeet/README.md
@@ -1,1 +1,5 @@
-# Password Generator Starter Code
+# Passwords Gneration
+
+The Passwords Generation project generates a password between 8 & 128 characters, once you click the generate pass word button then you are directed to a series of promps asking what length and what types of diffrent characters you'd like to have in your password. once answered it generates a custom password to your specifications and it will either displayed if you have done everything correctly or will alert you to try again.
+
+
